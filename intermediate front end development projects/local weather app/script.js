e053af3077c0b09014dddd718e2eb9809c5756a3
Corrina Chow <corrinayychow@gmail.com>
2018-02-03 00:43:33 -0500

    adds weather retrieval

--- intermediate front end development projects/local weather app/script.js
+++ intermediate front end development projects/local weather app/script.js
@@ -1,15 +1,29 @@
 var api = "https://fcc-weather-api.glitch.me/api/current?";
-var lat, lon;
 
 $( document ).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      lat = "lat=" + position.coords.latitude;
-      lon = "lon=" + position.coords.longitude;
-      $("#tempUnit").text(lat);
+      var lat = "lat=" + position.coords.latitude;
+      var lon = "lon=" + position.coords.longitude;
+      getWeather(lat, lon);
     });
   } else {
     console.log("Geolocation not available.")
   }
+});
 
-})
+
+
+
+function getWeather(lat, lon) {
+  var urlAPI = api + lat + "&" + lon;
+  $.ajax({
+    url: urlAPI, success: function (data) {
+      $("#weather-icon").html("<img src='" + data.weather[0].icon+"'/>") //temp lol use http://erikflowers.github.io/weather-icons/
+      $("#location").text(data.name + ", " + data.sys.country);
+      $("#temperature").text(data.main.temp);
+    }
+  });
+}
+
+//var fullDate = new Date() //Sat Feb 03 2018 00:27:08 GMT-0500 (EST)
