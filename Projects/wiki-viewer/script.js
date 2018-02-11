01b0dec5010822464bc8ab83c292d713f517a2b8
corrina chow <corrina.yy.chow@gmail.com>
2018-02-10 21:27:54 -0500

    Fix query results returning previous search's output

--- Projects/wiki-viewer/script.js
+++ Projects/wiki-viewer/script.js
@@ -1,17 +1,18 @@
+
 $(document).ready(function() {
   $('#searchSubmit').click(function() {
+    var searchQuery = $('#searchString').val();
     $.ajax({
       type: 'GET',
-      url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&prop=extracts&exsentences=1&search=' + $('#searchString').val() + '&limit=8&callback=?',
+      url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&prop=extracts&exsentences=1&search=' + searchQuery + '&limit=8&callback=?',
       dataType: "json",
       success: processData,
     })
   });
 
   function processData(data) {
-    console.log(data);
+    $( ".entry" ).remove(); //clears current search results when new query is made
     for (i = 0; i < data[2].length; i++) {
-
       $('#resultViewer').append(
         '<div class="entry animate-bottom" style="display:block">' +
         '<a href="'+ data[3][i] + '">' +
@@ -19,17 +20,11 @@ $(document).ready(function() {
         data[2][i] + '</a>' +
         '</div>');
     }
-    $('.entry').fadeOut(0);
     $('.entry').fadeIn(500);
   }
 });
 
-
-
-
-
-
-$('input').keyup(function(event) { //pressing enter triggers api call
+$('input').keyup(function(event) { //pressing Enter key simulates click on search and makes API call
   if (event.keyCode == 13) {
     $('#searchSubmit').click();
   }
