e44d9b63b972511e41c76ec033a99cf1c12d6588
corrina chow <corrina.yy.chow@gmail.com>
2018-02-13 22:20:07 -0500

    Add CSS

--- Projects/twitch-json-api/script.js
+++ Projects/twitch-json-api/script.js
@@ -18,8 +18,8 @@ $(document).ready(function() {
 
       $.getJSON(getChannelStatus('channels', channel), data => {
         var logoURL = data.logo != null ? data.logo : 'https://static-cdn.jtvnw.net/user-default-pictures/bb97f7e6-f11a-4194-9708-52bf5a5125e8-profile_image-300x300.jpg',
-        name = data.display_name != null ? data.display_name : channel,
-        description = status == 'online' ? ': ' + data.status : '';
+        name = data.display_name != null ? data.display_name : channel;
+        description = status == 'online' ? ': ' + (data.status).substr(0,12) + '...' : '';
         var html = '<div class="row channel ' + status + '"><div class="col-xs-2 col-sm-3 text-center" id="logo"><img src="' + logoURL + '" class="logo"></div><div class="col-xs-10 col-sm-8 name" id="name"><a href="' + data.url + '" target="_blank">' + name + '</a></div><div class="col-xs-10 col-sm-8 game" id="streaming">' + game + '<span class="hidden-xs">' + description + '</span></div></div>';
         status === 'online' ? $('#viewer').prepend(html) : $('#viewer').append(html);
       });
@@ -29,12 +29,12 @@ $(document).ready(function() {
 
 $('.selector').on('click', function() {
   if (this.id == 'online') {
-    $('.online').fadeIn(500);
-    $('.offline').fadeOut(500);
+    $('.online').fadeIn(300);
+    $('.offline').fadeOut(300);
   } else if (this.id == 'offline') {
-    $('.online').fadeOut(500);
-    $('.offline').fadeIn(500);
+    $('.online').fadeOut(300);
+    $('.offline').fadeIn(300);
   } else if (this.id == 'all') {
-    $('.channel').fadeIn(500);
+    $('.channel').fadeIn(300);
   }
 });
