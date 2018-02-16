fa20d765eaedf92cc47c2529809309e483b3d0f5
corrina chow <corrina.yy.chow@gmail.com>
2018-02-15 19:16:35 -0500

    Finished Wherefore Art Though

---
+++ Algorithms/20-wherefore-art-though.js
@@ -0,0 +1,10 @@
+function whatIsInAName(collection, source) {
+  var sourceKeys = Object.keys(source);//returns keys of source as an array
+  console.log(sourceKeys);
+  return collection.filter(function(objects) {//returns all the elements in collections that return true
+    console.log(objects);
+    return sourceKeys.every(function (key) {//tests every element from sourceKeys (key)
+      return objects.hasOwnProperty(key) && objects[key] === source[key]; //tests if collection objects has the same key and value as the source key
+    });
+  });
+}
