8fdded0048f50fd7818732659b77f4a042620c66
corrinachow <corrina.yy.chow@gmail.com>
2018-02-19 00:50:23 -0500

    Remove tests

--- Algorithms/31-smallest-common-multiple.js
+++ Algorithms/31-smallest-common-multiple.js
@@ -5,44 +5,13 @@ function smallestCommons(arr) {
   for (i = Math.min(...arr); i <= Math.max(...arr); i++) {
     divisors.push(i)
   }
-while (!smallestMultiple) {
-  temp += Math.max(...arr);
-  count = 0;
-  divisors.forEach(divisor => {
-    if (temp % divisor == 0) count ++;
-  });
-  if (count == divisors.length) smallestMultiple += temp;
-}
+  while (!smallestMultiple) {
+    temp += Math.max(...arr);
+    count = 0;
+    divisors.forEach(divisor => {//Iterates through all divisors
+      if (temp % divisor == 0) count++;
+    });
+    if (count == divisors.length) smallestMultiple += temp;
+  }
   return smallestMultiple;
 }
-
-console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-console.log(smallestCommons([23, 18]));
-console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-/*
-while (smallestMultiple == 0) {
-    arr.forEach((n, index) => {
-        temp[index] = arr[index] + arr[index];
-        console.log('this is '+ temp)
-        count = 1;
-        divisors.forEach(divisor => {
-
-          console.log(divisor);
-          console.log(n);
-          if (n % divisor == 0) {
-            count++;
-
-            console.log('current count is :' +count)
-            console.log(n + ' is divisible by ' + divisor);
-          }
-
-          console.log('count is '+count)
-        if (count == divisors.length)
-          smallestMultiple = n
-        })
-
-
-
-      });
-      */
